7e60a057aa66114a327016a3255de627c8ae2995
limdp99 <dlim@pm-studios.com>
2023-05-31 14:32:50 +0900

    Updates

---
+++ precache-manifest.b19367017bfd082637e0ec47d89d5f77.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ec5613a93bd97e676b788adb1f364fd",
+    "url": "/redeem_opus/index.html"
+  },
+  {
+    "revision": "ba9899f6ba606aee2e3f",
+    "url": "/redeem_opus/static/css/main.4774758a.chunk.css"
+  },
+  {
+    "revision": "62e6916a0a8d657372fe",
+    "url": "/redeem_opus/static/js/2.8e925c60.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/redeem_opus/static/js/2.8e925c60.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba9899f6ba606aee2e3f",
+    "url": "/redeem_opus/static/js/main.d17df235.chunk.js"
+  },
+  {
+    "revision": "e807e02bf3c3fdfb1583",
+    "url": "/redeem_opus/static/js/runtime-main.1a963f99.js"
+  }
+]);

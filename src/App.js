1ed904f4a1b51b2f80c76c1b37685cc0707c335b
limdp99 <dlim@pm-studios.com>
2023-05-31 14:28:42 +0900

    init

---
+++ src/App.js
@@ -0,0 +1,134 @@
+import React, { Component } from 'react';
+import './App.css';
+import './index.css'
+
+import dataMap from './RedeemCode_Opus.json';
+
+const divStyle = {
+  width: '100%',
+  height: 'auto',
+  backgroundImage: 'url(./images/Bg_background.jpg)',
+  backgroundPosition: 'center',
+  backgroundSize: 'cover',
+  backgroundRepeat: 'no-repeat',
+};
+
+class App extends Component {
+	state = {
+		visibleDownloadLink: false,
+		visibleError: false,
+		redeemCode: "",
+	}
+
+	constructor(props){
+		super(props);
+		
+		this.updateInput = this.updateInput.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);
+	}
+	
+	updateInput(event){
+		this.setState({redeemCode : event.target.value});
+	}
+
+	handleSubmit(){
+		let result = dataMap.data.find(item => item === this.state.redeemCode);
+		
+		//console.log('Your input value is: ' + this.state.redeemCode);
+		if (result)
+		{
+			this.setState({ 
+				visibleDownloadLink: true,
+				visibleError: false
+			});
+		}
+		else
+		{
+			this.setState({ 
+				visibleDownloadLink: false,
+				visibleError: true
+			});
+		}
+	}
+
+	handleLink = () => {
+		
+	}
+
+	renderDownloadLink = () => (
+		<div className="redeem">
+			<p>Google Drive Link - <a href='https://drive.google.com/file/d/1m8HpinMzddtdEbFgmh396o02VUFdvkNn/view?usp=sharing' onClick={this.handleLink} target='_blank'>Download</a></p>
+		</div>
+	)
+	
+	renderErrorMsg = () => (
+		<div className="errmsg">
+			<p>Invalid Redeem Code</p>
+		</div>
+	)
+
+	handleSignin = () => {
+		
+	}
+	
+	handleChange(e) {
+		console.log(e);
+		return;
+		const { name, value } = e.target;
+		if(name === 'code') 
+			this.setState({ redeemCode: value });
+
+		const { changeInput } = this.props;
+		changeInput({ name, value });
+	}
+
+	render() {
+		return (
+			<div style={divStyle}>
+				<div className="frame">
+					<div className="top center">
+						<img src={'./images/Logo_opus.png'} alt='opus' />
+						<div className="title">
+							<h1>OPUS Bonus Digital Soundtrack Download</h1>
+						</div>
+					</div>
+					<div className="middle center">
+						<div className="text01">
+							Thank you for purchasing OPUS. This is the Downloadable Soundtrack page to redeem your bonus<br/> 
+							voucher included with first print copies for PlayStation 4 and/or Nintendo Switch.
+						</div>
+						<div className="text02">
+							**This is not to purchase the actual game. Please do not purchase this item as it is non-refundable<br/> 
+							and only to download the digital soundtrack. The purpose of this page is for Downloadable voucher<br/> 
+							redemption included with certain retail copies.**
+						</div>
+						<div className="text03">
+							<div className="padding-l-32">
+								Please note:<br/> 
+								<br/>
+								• Bonus vouchers are only available in first print copies and brand new factory sealed copies.<br/> 
+								  We are not responsible for opened merchandise. <br/> 
+								• Please download to your PC. <br/> 
+								• A zip file containing sound files will be downloaded. <br/> 
+								• Cannot be re-issued. No returns accepted, non-transferable unless stated otherwise.
+							</div>
+						</div>
+						<div className="signup margin-b-16">
+							<form>
+								<h2>Enter Redeem Code</h2>
+								<input type="text" name="code" placeholder="Redeem Code" onChange={this.updateInput} onKeyPress={this.updateInput} maxLength="10" required />
+								<div onClick={this.handleSubmit} className="submit">
+									<p>Submit</p>
+								</div>
+							</form>
+						</div>
+						{this.state.visibleError && this.renderErrorMsg()}
+						{this.state.visibleDownloadLink && this.renderDownloadLink()}
+					</div>
+				</div>
+			</div>
+		);
+	}
+}
+
+export default App;
